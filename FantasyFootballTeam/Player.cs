c1a576ab27caa6a05b9f0df097dbdfa0d257cca4
Sean <seanm.oneill94@gmail.com>
2020-09-02 23:33:16 +0100

    added classes

--- FantasyFootballTeam/Player.cs
+++ FantasyFootballTeam/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FantasyFootballTeam
@@ -10,9 +11,9 @@ namespace FantasyFootballTeam
         public string Name { get; set; }
         public string FullName { get; set; }
         public string Team { get; set; }
-        public string Position { get; set; }
+        public Position Position { get; set; }
         public string Available { get; set; }
-        public string Price { get; set; }
+        public decimal Price { get; set; }
         public string Uncertainty { get; set; }
         public string OverallPoints { get; set; }
         public string Week1 { get; set; }
@@ -21,5 +22,76 @@ namespace FantasyFootballTeam
         public string Week4 { get; set; }
         public string Week5 { get; set; }
         public string Week6 { get; set; }
+    }
+
+    public class Team {
+
+        public string TeamName { get; set; }
+        public List<Player> Players {get; set;}
+
+        public List<Player> GetPlayersByPosition(Position position) {
+            return Players.Where(p => p.Position == position).ToList();
+        }
+    }
+
+    public enum Position {
+        Goalkeeper,
+        Defender,
+        Midfielder,
+        Forward,
+    }
+
+    public class Squad {
+
+        public Squad(int gk, int def, int mid, int fwd) {
+            GoalkeeperCount = gk;
+            DefenderCount = def;
+            MidfielderCount = mid;
+            ForwardCount = fwd;
+        }
+
+        public readonly int GoalkeeperCount;
+        public readonly int DefenderCount;
+        public readonly int MidfielderCount;
+        public readonly int ForwardCount;
+
+        public List<Player> Goalkeepers;
+        public List<Player> Defenders;
+        public List<Player> Midfielders;
+        public List<Player> Forwards;
+
+        public bool CanAddPlayer(Position position) {
+            switch (position) {
+                case Position.Goalkeeper:
+                    return Goalkeepers.Count < GoalkeeperCount;
+                case Position.Defender:
+                    return Defenders.Count < DefenderCount;
+                case Position.Midfielder:
+                    return Midfielders.Count < MidfielderCount;
+                case Position.Forward:
+                    return Forwards.Count < ForwardCount;
+                default:
+                    return false;
+            }
+        }
+
+        public void AddPlayer(Position position, Player player)
+        {
+            switch (position)
+            {
+                case Position.Goalkeeper:
+                    Goalkeepers.Add(player);
+                    break;
+                case Position.Defender:
+                    Defenders.Add(player);
+                    break;
+                case Position.Midfielder:
+                    Midfielders.Add(player);
+                    break;
+                case Position.Forward:
+                    Forwards.Add(player);
+                    break;
+            }
+        }
     }
 }
--- FantasyFootballTeam/Program.cs
+++ FantasyFootballTeam/Program.cs
@@ -1,29 +1,67 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
+using System.Linq;
 
 namespace FantasyFootballTeam
 {
     public class Program
     {
+        private const float Money = 100;
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             var players = LoadJson();
+            var teams = GetTeams(players);
+
 
             Console.WriteLine($"{players.Count}");
+
+            var squad = new Squad(2, 5, 5, 3);
         }
 
         public static List<Player> LoadJson()
         {
             List<Player> players;
-            using (StreamReader r = new StreamReader(@"C:\Development\FantasyFootball\PlayersComplete.json"))
+            using (StreamReader r = new StreamReader(@"C:\Development\FantasyFootball\FantasyFootballTeam\PlayersComplete.json"))
             {
                 string json = r.ReadToEnd();
                 players = JsonConvert.DeserializeObject<List<Player>>(json);
             }
             return players;
         }
+
+
+        private static List<Team> GetTeams(List<Player> allPlayers) {
+            var teams = new List<Team>();
+
+            foreach (var player in allPlayers) {
+                if (teams.Any(t => t.TeamName == player.Team))
+                {
+                    teams.First(t => t.TeamName == player.Team).Players.Add(player);
+                }
+                else {
+                    teams.Add(new Team()
+                    {
+                        TeamName = player.Team,
+                        Players = new List<Player>() { player }
+                    });
+                }
+            
+            
+            }
+
+
+            return null;
+        }
+
+        /*
+        private Player SelectCheapestWithMostPoints(Players players) { 
+            players.
+            
+        }
+        */
     }
 }
