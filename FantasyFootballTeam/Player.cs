dd3e7f57bfd9d4e754212381e84037fcf6d0c086
Sean <seanm.oneill94@gmail.com>
2020-09-02 19:37:18 +0100

    init commit

---
+++ FantasyFootballTeam/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FantasyFootballTeam
+{
+    public class Player
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Team { get; set; }
+        public string Position { get; set; }
+        public string Available { get; set; }
+        public string Price { get; set; }
+        public string Uncertainty { get; set; }
+        public string OverallPoints { get; set; }
+        public string Week1 { get; set; }
+        public string Week2 { get; set; }
+        public string Week3 { get; set; }
+        public string Week4 { get; set; }
+        public string Week5 { get; set; }
+        public string Week6 { get; set; }
+    }
+}
---
+++ FantasyFootballTeam/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FantasyFootballTeam
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var players = LoadJson();
+
+            Console.WriteLine($"{players.Count}");
+        }
+
+        public static List<Player> LoadJson()
+        {
+            List<Player> players;
+            using (StreamReader r = new StreamReader(@"C:\Development\FantasyFootball\PlayersComplete.json"))
+            {
+                string json = r.ReadToEnd();
+                players = JsonConvert.DeserializeObject<List<Player>>(json);
+            }
+            return players;
+        }
+    }
+}
